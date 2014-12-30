fe3845840fbc31cd09ca88e556ab5f2941b7d071
Jonath <nonis.b@gmail.com>
2014-12-30 12:24:59 +0100

    define works for values.

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -1,9 +1,8 @@
 use evalitem::EvalItem;
 use environment::Environment;
 
-pub fn evaluate(item_init: EvalItem, env: &mut Box<Environment>) -> EvalItem {
+pub fn evaluate(item: EvalItem, env: &mut Box<Environment>) -> EvalItem {
     let mut loops = 0i;
-    let mut item = item_init;
     loop {
         if loops > 100 { panic!("Max recursion reached!"); }
 
@@ -25,6 +24,19 @@ pub fn evaluate(item_init: EvalItem, env: &mut Box<Environment>) -> EvalItem {
                                 let mut quoted = list.clone();
                                 quoted.remove(0);
                                 return EvalItem::List(quoted);
+                            },
+                            "define" => {
+                                if list.len() != 3 {
+                                    panic!("define takes 2 arguments.");
+                                }
+                                let item_to_evaluate = list[2].clone();
+                                let item_to_add = evaluate(item_to_evaluate, env);
+                                let name_to_add = match list[1].clone() {
+                                    EvalItem::Value(val) => val,
+                                    _ => panic!("Must give string as name!"),
+                                };
+                                env.add(name_to_add.as_slice(), item_to_add);
+                                return EvalItem::Empty;
                             },
                             _ => panic!("Unknown keyword!"),
                         }
@@ -63,5 +75,16 @@ mod test {
             EvalItem::Value("1".to_string()),
             EvalItem::Value("2".to_string()))),
                    evaluate(item, &mut env));
+    }
+
+    #[test]
+    fn evaluate_define() {
+        let mut env = box Environment { vars: HashMap::new(), outer: None };
+        let item = EvalItem::List(vec!(
+            EvalItem::Value("define".to_string()),
+            EvalItem::Value("one".to_string()),
+            EvalItem::Value("einz".to_string())));
+        assert_eq!(EvalItem::Empty, evaluate(item, &mut env));
+        assert_eq!(EvalItem::Value("einz".to_string()), *env.find_value("one").unwrap());
     }
 }
