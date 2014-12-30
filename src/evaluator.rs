f8775ee1cb873c77ee730299a60d963c8a50764c
Jonath <nonis.b@gmail.com>
2014-12-30 11:20:35 +0100

    quote implemented.

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -11,17 +11,28 @@ pub fn evaluate(item_init: EvalItem, env: &mut Box<Environment>) -> EvalItem {
             EvalItem::Value(ref value) => {
                 match env.find_environment_with_var(value.as_slice()) {
                     Some(env_with_var) =>
-                        return env_with_var.find_value(value.as_slice())
-                        .unwrap().clone(),
+                        return env_with_var.find_value(value.as_slice()).unwrap().clone(),
                     None => return item.clone(),
                 }
             },
-            EvalItem::List(n) => {
-                panic!("TODO!");
+            EvalItem::List(ref list) => {
+                if list.is_empty() { return EvalItem::Empty; }
+                match list[0] {
+                    EvalItem::Value(ref keyword) => {
+                        match keyword.as_slice() {
+                            "quote" => {
+                                if list.len() == 1 { return EvalItem::Empty; }
+                                let mut quoted = list.clone();
+                                quoted.remove(0);
+                                return EvalItem::List(quoted);
+                            },
+                            _ => panic!("Unknown keyword!"),
+                        }
+                    },
+                    _ => panic!("Must start list evaluation with keyword!"),
+                }                        
             },
-            _ => {
-                panic!("Shouldn't get here!");
-            }
+            _ => panic!("Shouldn't get here!"),            
         }
     }
 }
@@ -38,6 +49,19 @@ mod test {
         let mut env = box Environment { vars: HashMap::new(), outer: None };
         let item = EvalItem::Value("ping".to_string());
         assert_eq!(EvalItem::Value("ping".to_string()),
+                   evaluate(item, &mut env));
+    }
+
+    #[test]
+    fn evaluate_quote() {
+        let mut env = box Environment { vars: HashMap::new(), outer: None };
+        let item = EvalItem::List(vec!(
+            EvalItem::Value("quote".to_string()),
+            EvalItem::Value("1".to_string()),
+            EvalItem::Value("2".to_string())));
+        assert_eq!(EvalItem::List(vec!(
+            EvalItem::Value("1".to_string()),
+            EvalItem::Value("2".to_string()))),
                    evaluate(item, &mut env));
     }
 }
